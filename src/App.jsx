e2ae63073a23693f518e1a465e54530b906a18e3
YuzuyOS <hendriawan0725@gmail.com>
2025-06-05 11:54:51 +0800

    update

--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,9 @@ const App = () => {
   return (
     <>
       <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
-        {/* <Header /> */}
+        <Header />
         <Hero />
-        {/* <Benefits /> */}
+        <Benefits />
         {/* <Collaboration /> */}
         {/* <Services /> */}
         {/* <Pricing /> */}
